c9c8c32e4590d6fd3bd81f7452953a6c0f6bc45b
LiChunYan <1196298163@qq.com>
2018-11-18 17:19:24 +0800

    update the files

---
+++ src/entity/MyFile.java
@@ -0,0 +1,63 @@
+package entity;
+
+import java.util.Date;
+
+public class MyFile {
+	private int FileId;
+	public int getFileId() {
+		return FileId;
+	}
+	public void setFileId(int fileId) {
+		FileId = fileId;
+	}
+	private String fileName;
+	private boolean isDownloadable;
+	private Date uploadTime;
+	private int folderId;
+	private long size;
+	private String location;
+	private String fileType;
+	public String getFileType() {
+		return fileType;
+	}
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public boolean isDownloadable() {
+		return isDownloadable;
+	}
+	public void setDownloadable(boolean isDownloadable) {
+		this.isDownloadable = isDownloadable;
+	}
+	public Date getUploadTime() {
+		return uploadTime;
+	}
+	public void setUploadTime(Date uploadTime) {
+		this.uploadTime = uploadTime;
+	}
+	public int getFolderId() {
+		return folderId;
+	}
+	public void setFolderId(int folderId) {
+		this.folderId = folderId;
+	}
+	public long getSize() {
+		return size;
+	}
+	public void setSize(long size) {
+		this.size = size;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+}
