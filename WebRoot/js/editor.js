91f2b3cceb59bcd106ac02674b7b9e438b5a0326
LiChunYan <1196298163@qq.com>
2018-11-25 22:37:56 +0800

    delete the wrongly structured files directories

--- WebRoot/js/editor.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-$(function(){
-	/*创建富文本域*/
-	var E = window.wangEditor;
-    var editor = new E('#editor');
-    editor.create();
-})
--- src/services/OperationService.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public interface OperationService {
-	Boolean add(Object o);
-	Boolean delete(int id);
-	Boolean modify(Object o);
-}
